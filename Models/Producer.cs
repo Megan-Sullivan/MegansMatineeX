4aea3415dff7c3fd4a5f5f9f9eacd2810079edff
Megan <830037820@wwcc.edu>
2022-12-10 15:21:01 -0800

    Final Project Submission

--- Models/Producer.cs
+++ Models/Producer.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace MegansMatineeX.Models
 {
@@ -16,6 +18,11 @@ namespace MegansMatineeX.Models
         [Display(Name = "Producer Name")]
         [StringLength(50)]
         public string ProducerName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Establishment Date")]
+        public DateTime EstablishmentDate { get; set; }
 
         public ICollection<Movie> Movies { get; set; }
         public SiteAssignment SiteAssignment { get; set; }
